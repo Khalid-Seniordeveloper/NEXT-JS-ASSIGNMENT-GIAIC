31bb01a247753760884a537eb5c0f0fb8d6ff868
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-10-08 13:10:27 +0500

    1ST COMMIT

---
+++ assignment_04/app/page.tsx
@@ -0,0 +1,102 @@
+
+function Home(){
+  return(
+    <div>
+<div ><h1 className="text-center head ">ALL BLOGS !</h1></div>
+
+<div className="main-blog-container  flex justify-evenly mt-52">
+
+
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+
+</div>
+
+
+<div className="main-blog-container flex justify-evenly mt-11">
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+<div className="blog-container slide-top">
+
+<div className="image-container"></div>
+<div className="blog-title"><h1>Title one</h1></div>
+<div className="blog-description"><h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio dicta mollitia ipsam consectetur nesciunt, earum similique aspernatur labore magni nemo?</h3></div>
+<div  className="view"><button>View More</button></div>
+
+
+</div>
+
+
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default Home
