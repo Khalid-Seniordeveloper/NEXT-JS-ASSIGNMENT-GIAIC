aecf00e5b2eae6255ef99b13c2362fc120b34101
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-10-09 01:13:10 +0500

    navbar

---
+++ assignment_04/app/Component/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+<div className='nav-container flex items-center justify-evenly'>
+
+<div><h1 className='text-4xl name'>Blogging App</h1></div>
+
+<div>
+
+<ul className='list-none flex justify-center gap-20 text-4xl nav-list'>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact Us</li>
+
+</ul>
+
+</div>
+
+
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
